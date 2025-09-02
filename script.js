980ccd370e88db325f283b90aeebe811280ebf06
rhk998 <130037816+rhk998@users.noreply.github.com>
2025-09-02 18:26:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-//your code here!
+let current = 1;
 
+function gen(start, count) {
+  const list = document.getElementById('infi-list');
+
+  for (let i = 0; i < count; i++) {
+    const li = document.createElement('li');
+    li.textContent = `Item ${start + i}`;
+    list.appendChild(li);
+  }
+
+  current += count;
+}
+
+gen(current, 10);
+
+const listContainer = document.getElementById('infi-list');
+
+listContainer.addEventListener('scroll', () => {
+  if (
+    listContainer.scrollTop + listContainer.clientHeight >= listContainer.scrollHeight - 5
+  ) {
+    gen(current, 2);
+  }
+});
